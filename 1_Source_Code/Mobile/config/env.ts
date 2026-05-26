f4298034f666984682b2c9207694554550c6c4a0
Chau Ngoc Hoi <ngochoinct@gmail.com>
2026-05-26 15:08:20 +0700

    chore: exclude Mobile app, backups, and env files for new repository

--- 1_Source_Code/Mobile/config/env.ts
+++
@@ -1,6 +0,0 @@
-
-export const API_URL = `http://192.168.0.252:5000/api`;
-//export const API_URL = `https://kltn-gps-api.onrender.com/api`;
-
-export const SOCKET_URL = `http://192.168.0.252:5000`;
-//export const SOCKET_URL = `https://kltn-gps-api.onrender.com`;
